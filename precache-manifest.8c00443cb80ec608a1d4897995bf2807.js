1b6e68e09c013dbc4581c67511609e96d58ab044
Stud Horse <lalalatim@yahoo.com.tw>
2020-07-23 18:20:37 +0800

    Updates

---
+++ precache-manifest.8c00443cb80ec608a1d4897995bf2807.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c110032f2b254c20aa80d68d440b0d0d",
+    "url": "/dailychucknorris/index.html"
+  },
+  {
+    "revision": "dfa5c96b0d73ab0be5ba",
+    "url": "/dailychucknorris/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "50d65bc2985fd967ed7c",
+    "url": "/dailychucknorris/static/css/main.d4b8d7d9.chunk.css"
+  },
+  {
+    "revision": "dfa5c96b0d73ab0be5ba",
+    "url": "/dailychucknorris/static/js/2.518750e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dailychucknorris/static/js/2.518750e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50d65bc2985fd967ed7c",
+    "url": "/dailychucknorris/static/js/main.e79b94d4.chunk.js"
+  },
+  {
+    "revision": "80ee01d07339fcd173bc",
+    "url": "/dailychucknorris/static/js/runtime-main.9db07288.js"
+  }
+]);
